36a21d409f9f80c56488bff33ca23f783d3c14df
bornthenord <bornthenord@gmail.com>
2025-12-01 21:36:43 +0300

    keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,5 +1,18 @@
 local telescope = require("telescope.builtin")
 
-vim.keymap.set("n", "<leader>ff", telescope.find_files, { desc = "[F]ind [F]iles" })
-vim.keymap.set("n", "<leader>fb", telescope.buffers, { desc = "[F]ind [B]uffers" })
-vim.keymap.set("n", "<leader>fg", telescope.live_grep, { desc = "[F]ind [G]rep" })
+-- exit
+
+vim.keymap.set("n", "<leader>qq", ":q!<CR>")
+
+-- files
+vim.keymap.set("n", "<leader>ff", telescope.find_files, { desc = "[f]ind [f]iles" })
+vim.keymap.set("n", "<leader>fb", telescope.buffers, { desc = "[f]ind [b]uffers" })
+vim.keymap.set("n", "<leader>fg", telescope.live_grep, { desc = "[f]ind [g]rep" })
+
+-- buffers
+vim.keymap.set("n", "<Tab>", ":bnext<CR>", { desc = "[b]uffer [n]ext" })
+vim.keymap.set("n", "<S-Tab>", ":bprevious<CR>", { desc = "[b]uffer [p]rev" })
+vim.keymap.set("n", "<leader>bc", ":bufdo bd<CR>", { desc = "[b]uffers [c]lose" })
+vim.keymap.set("n", "<leader>bs", ":w<CR>", { desc = "[s]uffer [s]ave" })
+vim.keymap.set("n", "<leader>bq", ":bd<CR>", { desc = "[b]uffer [q]uit" })
+vim.keymap.set("n", "<leader>bQ", ":bd!<CR>", { desc = "[b]uffer [q]uit without save" })
