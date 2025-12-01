e9ed5d2250283453fbe6468864eed264a56cf7fd
bornthenord <bornthenord@gmail.com>
2025-12-01 22:11:29 +0300

    fix

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -9,10 +9,9 @@ vim.keymap.set("n", "<leader>ff", telescope.find_files, { desc = "[f]ind [f]iles
 vim.keymap.set("n", "<leader>fb", telescope.buffers, { desc = "[f]ind [b]uffers" })
 vim.keymap.set("n", "<leader>fg", telescope.live_grep, { desc = "[f]ind [g]rep" })
 
--- buffers
+-- buffer
 vim.keymap.set("n", "<Tab>", ":bnext<CR>", { desc = "[b]uffer [n]ext" })
 vim.keymap.set("n", "<S-Tab>", ":bprevious<CR>", { desc = "[b]uffer [p]rev" })
 vim.keymap.set("n", "<leader>bc", ":bufdo bd<CR>", { desc = "[b]uffers [c]lose" })
-vim.keymap.set("n", "<leader>bs", ":w<CR>", { desc = "[s]uffer [s]ave" })
-vim.keymap.set("n", "<leader>bq", ":bd<CR>", { desc = "[b]uffer [q]uit" })
-vim.keymap.set("n", "<leader>bQ", ":bd!<CR>", { desc = "[b]uffer [q]uit without save" })
+vim.keymap.set("n", "<leader>bw", ":w<CR>", { desc = "[s]uffer [w]rite" })
+vim.keymap.set("n", "<leader>bd", ":bd<CR>", { desc = "[b]uffer [d]elete" })
