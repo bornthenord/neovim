5417cbaab39aed3ef9c00d0fbed47a0cfec6429f
bornthenord <bornthenord@gmail.com>
2025-12-01 13:43:52 +0300

    telescop kaeymap

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,3 +1,5 @@
 local telescope = require("telescope.builtin")
 
 vim.keymap.set("n", "<leader>ff", telescope.find_files, { desc = "[F]ind [F]iles" })
+vim.keymap.set("n", "<leader>fb", telescope.buffers, { desc = "[F]ind [B]uffers" })
+vim.keymap.set("n", "<leader>fg", telescope.live_grep, { desc = "[F]ind [G]rep" })
