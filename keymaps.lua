e8d764c29283ed89391adebc941cdf0e0dee5fd0
bornthenord <bornthenord@gmail.com>
2025-12-02 12:42:26 +0300

    key remape

--- keymaps.lua
+++ keymaps.lua
@@ -8,10 +8,11 @@ vim.keymap.set("n", "qq", ":q!<CR>")
 vim.keymap.set("n", "ff", telescope.find_files, { desc = "[f]ind [f]iles" })
 vim.keymap.set("n", "fb", telescope.buffers, { desc = "[f]ind [b]uffers" })
 vim.keymap.set("n", "fg", telescope.live_grep, { desc = "[f]ind [g]rep" })
+vim.keymap.set("n", "ft", ":Neotree<CR>", { desc = "[f]ind [g]rep" })
+vim.keymap.set("n", "fs", ":w<CR>", { desc = "[f]ile [s]save" })
 
 -- buffer
 vim.keymap.set("n", "<Tab>", ":bnext<CR>", { desc = "[b]uffer [n]ext" })
 vim.keymap.set("n", "<S-Tab>", ":bprevious<CR>", { desc = "[b]uffer [p]rev" })
 vim.keymap.set("n", "<Esc>", ":bd<CR>", { desc = "[b]uffer [d]elete" })
-vim.keymap.set("n", "bc", ":bufdo bd<CR>", { desc = "[b]uffers [c]lose" })
-vim.keymap.set("n", "bw", ":w<CR>", { desc = "[s]uffer [w]rite" })
+vim.keymap.set("n", "<S-Esc>", ":bufdo bd<CR>", { desc = "[b]uffers [c]lose" })
