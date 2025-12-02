cc295171a64a7ca8feba6ca0e6d008f277b33fb5
bornthenord <bornthenord@gmail.com>
2025-12-02 22:51:52 +0300

    quickfix keymap

--- lua/dev/keymaps.lua
+++ lua/dev/keymaps.lua
@@ -6,3 +6,7 @@ end, { desc = "next error" })
 vim.keymap.set("n", "<leader>ep", function()
 	vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.ERROR })
 end, { desc = "prev error" })
+
+-- quickfix
+vim.keymap.set("n", "<leader>qf", vim.diagnostic.setqflist, { buffer = bufnr, desc = "Diagnostics to quickfix" })
+vim.keymap.set("n", "<leader>qa", vim.lsp.buf.code_action, { buffer = bufnr, desc = "Code actions" })
