0e34f625c4c5c762c5b59efa1a465aac92c2b382
bornthenord <bornthenord@gmail.com>
2025-12-02 23:02:39 +0300

    telescope filter

--- lua/core/plugins/telescope.lua
+++ lua/core/plugins/telescope.lua
@@ -3,4 +3,28 @@ return {
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 	},
+	require("telescope").setup({
+		pickers = {
+			find_files = {
+				find_command = {
+					"rg",
+					"--files",
+					"--hidden",
+					"--ignore-vcs",
+					"--glob",
+					"!**/bin/**",
+					"--glob",
+					"!**/obj/**",
+					"--glob",
+					"!**/Debug/**",
+					"--glob",
+					"!**/Release/**",
+					"--glob",
+					"!**/.vs/**",
+					"--glob",
+					"!**/packages/**",
+				},
+			},
+		},
+	}),
 }
