6445bfc02fbd5c456d60cad6c8c71c1baeb5b5b8
bornthenord <bornthenord@gmail.com>
2025-12-02 22:21:19 +0300

    test mapping

--- lua/dev/dotnet/keymaps.lua
+++ lua/dev/dotnet/keymaps.lua
@@ -4,3 +4,7 @@ local prefix = "<leader>p"
 vim.keymap.set("n", prefix .. "b", function()
 	toggleterm.exec("dotnet build")
 end, { desc = "Build project" })
+
+vim.keymap.set("n", prefix .. "t", function()
+	toggleterm.exec("dotnet test")
+end, { desc = "Build project" })
