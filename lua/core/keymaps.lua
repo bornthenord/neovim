80009c341e89ad40965880b395b83b6969074853
bornthenord <bornthenord@gmail.com>
2025-12-02 11:48:17 +0300

    file tree keymap

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -2,16 +2,17 @@ local telescope = require("telescope.builtin")
 
 -- exit
 
-vim.keymap.set("n", "<leader>qq", ":q!<CR>")
+vim.keymap.set("n", "qq", ":q!<CR>")
 
 -- files
-vim.keymap.set("n", "<leader>ff", telescope.find_files, { desc = "[f]ind [f]iles" })
-vim.keymap.set("n", "<leader>fb", telescope.buffers, { desc = "[f]ind [b]uffers" })
-vim.keymap.set("n", "<leader>fg", telescope.live_grep, { desc = "[f]ind [g]rep" })
+vim.keymap.set("n", "ff", telescope.find_files, { desc = "[f]ind [f]iles" })
+vim.keymap.set("n", "fb", telescope.buffers, { desc = "[f]ind [b]uffers" })
+vim.keymap.set("n", "fg", telescope.live_grep, { desc = "[f]ind [g]rep" })
+vim.keymap.set("n", "ft", ":Telescope file_browser path=%:p:h<CR>", { desc = "[f]ind [t]ree" })
 
 -- buffer
 vim.keymap.set("n", "<Tab>", ":bnext<CR>", { desc = "[b]uffer [n]ext" })
 vim.keymap.set("n", "<S-Tab>", ":bprevious<CR>", { desc = "[b]uffer [p]rev" })
-vim.keymap.set("n", "<leader>bc", ":bufdo bd<CR>", { desc = "[b]uffers [c]lose" })
-vim.keymap.set("n", "<leader>bw", ":w<CR>", { desc = "[s]uffer [w]rite" })
-vim.keymap.set("n", "<leader>bd", ":bd<CR>", { desc = "[b]uffer [d]elete" })
+vim.keymap.set("n", "<Esc>", ":bd<CR>", { desc = "[b]uffer [d]elete" })
+vim.keymap.set("n", "bc", ":bufdo bd<CR>", { desc = "[b]uffers [c]lose" })
+vim.keymap.set("n", "bw", ":w<CR>", { desc = "[s]uffer [w]rite" })
--- lua/core/plugins/telescope.lua
+++ lua/core/plugins/telescope.lua
@@ -2,5 +2,15 @@ return {
 	"nvim-telescope/telescope.nvim",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
+		"nvim-telescope/telescope-file-browser.nvim",
+	},
+	opts = {
+		extensions = {
+			file_browser = {
+				-- Ваши настройки расширения (по желанию)
+				hijack_netrw = true, -- Заменяет стандартный Netrw
+				theme = "ivy", -- Выбор темы (опционально)
+			},
+		},
 	},
 }
