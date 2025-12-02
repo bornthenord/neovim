c6dcec7fa8dccf2389a166de658a284d28023653
bornthenord <bornthenord@gmail.com>
2025-12-02 23:26:27 +0300

    navigation keymaps

--- lua/dev/keymaps.lua
+++ lua/dev/keymaps.lua
@@ -7,6 +7,23 @@ vim.keymap.set("n", "<leader>ep", function()
 	vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.ERROR })
 end, { desc = "prev error" })
 
+vim.keymap.set("n", "<leader>wn", function()
+	vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.WARNING })
+end, { desc = "next error" })
+
+vim.keymap.set("n", "<leader>wp", function()
+	vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.WARNING })
+end, { desc = "prev error" })
+
 -- quickfix
 vim.keymap.set("n", "<leader>qf", vim.diagnostic.setqflist, { buffer = bufnr, desc = "Diagnostics to quickfix" })
 vim.keymap.set("n", "<leader>qa", vim.lsp.buf.code_action, { buffer = bufnr, desc = "Code actions" })
+
+-- navigation
+vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = bufnr, desc = "Go to definition" })
+vim.keymap.set("n", "gb", "<C-o>", { desc = "Jump back" })
+vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { buffer = bufnr, desc = "Go to implementation" })
+vim.keymap.set("n", "gr", vim.lsp.buf.references, { buffer = bufnr, desc = "Find references" })
+
+-- refactoring
+vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { buffer = bufnr, desc = "Rename" })
