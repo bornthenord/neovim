4632e80f37dd2fc5c169997a5f32d7cd27d5d6fd
bornthenord <bornthenord@gmail.com>
2025-12-03 19:58:50 +0300

    neotree -> nvimtree

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -7,7 +7,7 @@ vim.keymap.set("n", "<leader>qq", ":q!<CR>", { desc = "exit" })
 vim.keymap.set("n", "ff", telescope.find_files, { desc = "find files" })
 vim.keymap.set("n", "fb", telescope.buffers, { desc = "find buffers" })
 vim.keymap.set("n", "fg", telescope.live_grep, { desc = "find grep" })
-vim.keymap.set("n", "ft", ":Neotree<CR>", { desc = "find grep" })
+vim.keymap.set("n", "ft", ":NvimTreeOpen<CR>", { desc = "find grep" })
 vim.keymap.set("n", "fs", ":w<CR>", { desc = "file save" })
 
 -- buffer
