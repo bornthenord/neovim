af79d21a8bbe813109cda80c479d919739aa1d6f
bornthenord <bornthenord@gmail.com>
2025-12-04 20:13:31 +0300

    tree keymap

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -7,8 +7,9 @@ vim.keymap.set("n", "<leader>qq", ":q!<CR>", { desc = "exit" })
 vim.keymap.set("n", "ff", telescope.find_files, { desc = "find files" })
 vim.keymap.set("n", "fb", telescope.buffers, { desc = "find buffers" })
 vim.keymap.set("n", "fg", telescope.live_grep, { desc = "find grep" })
-vim.keymap.set("n", "ft", ":NvimTreeOpen<CR>", { desc = "find grep" })
 vim.keymap.set("n", "fs", ":w<CR>", { desc = "file save" })
+vim.keymap.set("n", "ft", ":NvimTreeOpen<CR>", { desc = "Open" })
+vim.keymap.set("n", "<Esc>", ":NvimTreeOpen<CR>", { buffer = bufnr, desc = "Go back to tree" })
 
 -- buffer
 vim.keymap.set("n", "<Tab>", ":bnext<CR>", { desc = "buffer next" })
--- lua/core/plugins/nvim-tree.lua
+++ lua/core/plugins/nvim-tree.lua
@@ -5,7 +5,22 @@ return {
     dependencies = {
         "nvim-tree/nvim-web-devicons",
     },
-    config = function()
-        require("nvim-tree").setup({})
-    end,
+    opts = {
+        update_focused_file = {
+            enable = true,
+        },
+        on_attach = function(bufnr)
+            local api = require("nvim-tree.api")
+
+            -- Default mappings
+            api.config.mappings.default_on_attach(bufnr)
+
+            -- Unbind a default keymap (e.g., <c-k>)
+            vim.keymap.del("n", "<c-k>", { buffer = bufnr })
+
+            -- Set a new keymap (e.g., use 'g' to open a file instead of 'o')
+            vim.keymap.set("n", "h", api.node.navigate.parent_close, { buffer = bufnr, desc = "Close file" })
+            vim.keymap.set("n", "l", api.node.open.edit, { buffer = bufnr, desc = "Open file" })
+        end,
+    },
 }
