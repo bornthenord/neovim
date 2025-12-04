c5deefd124093fb1818ab61a7d9ce8034f1cd14c
bornthenord <bornthenord@gmail.com>
2025-12-04 20:54:22 +0300

    typofix

--- lua/ui/init.lua
+++ lua/ui/init.lua
@@ -1,5 +1,4 @@
 vim.opt.number = true -- Номера строк
-vim.opt.cursorline = true -- Подсветка текущей строки
 vim.opt.termguicolors = true -- Включение true-цветов в терминале (важно для тем)
 vim.opt.cursorline = true
 vim.opt.colorcolumn = "100"
